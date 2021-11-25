397a5bf2a8c5f8c368536dbc58f7607dfba0c770
Abhishek <abhi.ay04@gmail.com>
2021-11-25 16:50:48 +0530

    heroku hosting

---
+++ students.js
@@ -0,0 +1,54 @@
+const students =
+    [{
+        "id": 1,
+        "first_name": "Jorgan",
+        "last_name": "Blankenship",
+        "email": "jblankenship0@ft.com"
+      }, {
+        "id": 2,
+        "first_name": "Wilmer",
+        "last_name": "Kabisch",
+        "email": "wkabisch1@dell.com"
+      }, {
+        "id": 3,
+        "first_name": "Rickie",
+        "last_name": "Dikelin",
+        "email": "rdikelin2@example.com"
+      }, {
+        "id": 4,
+        "first_name": "Bengt",
+        "last_name": "Kynder",
+        "email": "bkynder3@statcounter.com"
+      }, {
+        "id": 5,
+        "first_name": "Carolin",
+        "last_name": "Dixon",
+        "email": "cdixon4@harvard.edu"
+      }, {
+        "id": 6,
+        "first_name": "Denney",
+        "last_name": "Dowers",
+        "email": "ddowers5@paypal.com"
+      }, {
+        "id": 7,
+        "first_name": "Enrique",
+        "last_name": "Strange",
+        "email": "estrange6@list-manage.com"
+      }, {
+        "id": 8,
+        "first_name": "Morgen",
+        "last_name": "Yoakley",
+        "email": "myoakley7@usnews.com"
+      }, {
+        "id": 9,
+        "first_name": "Lyell",
+        "last_name": "Garey",
+        "email": "lgarey8@marriott.com"
+      }, {
+        "id": 10,
+        "first_name": "Lorant",
+        "last_name": "Parcells",
+        "email": "lparcells9@auda.org.au"
+      }]
+
+      module.exports = students
